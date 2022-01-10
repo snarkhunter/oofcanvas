240e356dae091d3ca7db738b95c8f1e456a408bb
Steve Langer <stephen.langer@nist.gov>
2022-01-10 15:39:00 -0500

    Code and comment cleanup.

--- oof2installlib.py
+++ oof2installlib.py
@@ -59,6 +59,7 @@ class oof_install_lib(install_lib.install_lib):
                                             stderr=subprocess.PIPE)
                     stdoutdata, stderrdata = proc.communicate()
                     if stderrdata:
+                        print >> sys.stderr, stderrdata
                         raise DistutilsExecError(
                             "Command failed: " + " ".join(cmd))
 
