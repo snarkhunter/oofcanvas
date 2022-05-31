fb4495aaf03955b6a6f11233ddc626bfa0529cc8
Steve Langer <stephen.langer@nist.gov>
2022-05-31 15:03:06 -0400

    Better reprs for swigged classes.  No more monkeypatching of gui canvas __init__.
    Instead of monkeypatching the __init__, which isn't possible when
    using the swig -builtin option, now the Python Canvas is derived
    from PythonCanvas, which is the swigged C++ class with extensions
    for Python.  It does make sense, really.

--- oofcanvas/oofcanvasgui/__init__.py
+++ oofcanvas/oofcanvasgui/__init__.py
@@ -13,4 +13,26 @@ if sys.version_info[0] < 3:
     from oofcanvasgui import *
 else:
     from .oofcanvasgui import *
-    
+
+# The actual python Canvas class is defined here.  The only thing that
+# it adds to the wrapped C++ PythonCanvas class is the Gtk.Layout and
+# functions related to it.  It's convenient to define the Gtk.Layout
+# from python so that it's treated the same way as other Gtk objects
+# created in python, without requiring us to know any of what that
+# actually entails.
+
+# If we ever have functions that return a PythonCanvas object from C++
+# to Python, then the PythonCanvas should keep a PyObject* that points
+# to its Canvas, so that only one Canvas object is associated with
+# each PythonCanvas object.
+
+class Canvas(PythonCanvas):
+    def __init__(self, width, height, ppu, **kwargs):
+        self.layout = Gtk.Layout(width_request=width, height_request=height,
+                            **kwargs)
+        PythonCanvas.__init__(self, self.layout, ppu)
+
+    def get_hadjustment(self):
+        return self.layout.get_hadjustment()
+    def get_vadjustment(self):
+        return self.layout.get_vadjustment()
