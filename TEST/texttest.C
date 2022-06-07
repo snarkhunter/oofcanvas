747bf51ca244d972e49b73a5e0f779233a17cf47
Steve Langer <stephen.langer@nist.gov>
2022-06-06 22:55:10 -0400

    Added comments, and a fudge factor that can be changed to affect the bug without changing the text size.
    The kerning is incorrect for small fonts if the ppu is large and the font is not monospaced.

--- TEST/texttest.C
+++ TEST/texttest.C
@@ -1,7 +1,23 @@
 // -*- C++ -*-
 
-// Just a demo program for drawing text on a GtkLayout.  This doesn't
-// use OOFCanvas.
+// A simple Cairo/Pango/Gtk3 program that demonstrates problems with
+// Pango, I think.
+
+// PPU is the pixels-per-unit used to define the Cairo scaling matrix.
+#define PPU 10.
+
+// FUDGE changes the pixels-per-unit, but also changes all lengths to
+// compensate.  Changing FUDGE should not change the display, in
+// principle.  But with PPU=10 and FUDGE=1, the kerning is incorrect
+// on small type.  With FUDGE=10 it's better.
+#define FUDGE 1.
+
+// The problem only occurs on proportionally spaced fonts.  Change
+// FONTNAME to "Courier" instead of "Times" and the display is correct
+// with FUDGE=1.
+#define FONTNAME "Times"
+
+//=\\=//=\\=//=\\=//=\\=//=\\=//=\\=//=\\=//
 
 #include <pango/pango.h>
 #include <pango/pangocairo.h>
@@ -12,44 +28,72 @@
 
 //=\\=//=\\=//=\\=//=\\=//=\\=//=\\=//=\\=//
 
-#define PIXELSIZE 200
-#define PPU 10.
+// Scroll offsets
+int offset_x = 0;
+int offset_y = 0;
 
-void realizeCB(GtkWidget *layout, gpointer) {
-  gtk_layout_set_size(GTK_LAYOUT(layout), PIXELSIZE, PIXELSIZE);
-}
+// Draw text at a given size and position.
 
 void textAt(const char *txt, double size, double x, double y,
 	    Cairo::RefPtr<Cairo::Context> context)
 {
   PangoLayout *lo = pango_cairo_create_layout(context->cobj());
   pango_layout_set_text(lo, txt, -1);
-  std::string fd = "Times " + std::to_string(size);
+  std::string fd =  std::string(FONTNAME) + " " + std::to_string(size*FUDGE);
   PangoFontDescription *pfd = pango_font_description_from_string(fd.c_str());
   pango_layout_set_font_description(lo, pfd);
   pango_font_description_free(pfd);
 
   context->save();
-  double baseline = pango_layout_get_baseline(lo)/double(PANGO_SCALE);
-  context->move_to(x, y+baseline);
+  context->move_to(x*FUDGE, y*FUDGE);
   pango_layout_context_changed(lo);
   pango_cairo_show_layout(context->cobj(), lo);
   g_object_unref(lo);
   context->restore();
 }
 
+// Draw event handler
 
 bool drawCB(GtkWidget *layout, Cairo::Context::cobject *ctxt, gpointer) {
   Cairo::RefPtr<Cairo::Context> context(new Cairo::Context(ctxt, false));
 
-  Cairo::Matrix transf(PPU, 0., 0., PPU, 0., 0.);
+  guint w,h;
+  gtk_layout_get_size(GTK_LAYOUT(layout), &w, &h);
+  
+  double x1, y1, x2, y2;
+  context->get_clip_extents(x1, y1, x2, y2);
+  gtk_layout_set_size(GTK_LAYOUT(layout), x2-x1, y2-y1);
+
+  Cairo::Matrix transf(PPU/FUDGE, 0., 0., PPU/FUDGE, offset_x, offset_y);
   context->set_matrix(transf);
 
-  textAt("1: hello", 1., 3, 3, context);
-  textAt("2: hello", 2,  10, 2, context);
-  textAt("5: hello", 5, 20, 5, context);
-  textAt("10: hello", 10, 0, 10, context);
-  textAt("15: hello", 15, 15, 20, context);
+  // Draw a 10x10 square for reference.
+  context->save();
+  context->move_to(0,0);
+  context->line_to(10*FUDGE, 0);
+  context->line_to(10*FUDGE, 10*FUDGE);
+  context->line_to(0, 10*FUDGE);
+  context->close_path();
+  context->set_source_rgb(1,0,0);
+  context->set_line_width(FUDGE/(PPU));
+  context->stroke();
+  context->restore();
+
+  textAt("1: hello", 1, 1, 1, context);
+  textAt("2: hello", 2, 1, 2, context);
+  textAt("5: hello", 5, 1, 5, context);
+  textAt("10: hello", 10, 1, 10, context);
+  textAt("15: hello", 15, 1, 20, context);
+  return false;
+}
+
+bool scrollCB(GtkWidget *layout, GdkEventScroll *event, gpointer)
+{
+  if(event->direction == GDK_SCROLL_SMOOTH) {
+    offset_x -= event->delta_x;
+    offset_y -= event->delta_y;
+    gtk_widget_queue_draw(layout);
+  }
   return false;
 }
 
@@ -71,11 +115,16 @@ int main(int argc, char *argv[]) {
   gtk_box_pack_start(GTK_BOX(vbox), frame, true, true, 2);
   gtk_frame_set_shadow_type(GTK_FRAME(frame), GTK_SHADOW_IN);
 
+  GtkWidget *swind = gtk_scrolled_window_new(nullptr, nullptr);
+  gtk_container_add(GTK_CONTAINER(frame), swind);
+  gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(swind),
+				      GTK_SHADOW_ETCHED_IN);
+								     
   GtkWidget *layout = gtk_layout_new(nullptr, nullptr);
-  gtk_container_add(GTK_CONTAINER(frame), layout);
-
-  g_signal_connect(G_OBJECT(layout), "realize",
-		   G_CALLBACK(realizeCB), nullptr);
+  gtk_container_add(GTK_CONTAINER(swind), layout);
+  gtk_widget_add_events(layout, (GdkEventMask) GDK_SCROLL_MASK);
+  g_signal_connect(G_OBJECT(layout), "scroll_event",
+		   G_CALLBACK(scrollCB), nullptr);
   g_signal_connect(G_OBJECT(layout), "draw",
 		   G_CALLBACK(drawCB), nullptr);
 
